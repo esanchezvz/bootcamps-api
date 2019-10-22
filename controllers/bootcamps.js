14c6d5a44166cb44f73e76bdbc6055ba56ebf2be
esanchezvz <esteban.sanvaz@gmail.com>
2019-10-21 22:12:22 -0500

    added advanced querying and sorting when getting all bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,40 @@ const geocoder = require('../utils/geocoder');
 // @route GET api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHanlder(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ['select', 'sort'];
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators like $gt, $gte, etc.
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Finding resource
+  let query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select fields to send in response
+  if (req.query.select) {
+    // const fields = req.query.select.split(',').join(' '); // turn into an array, then join with space
+    const fields = req.query.select.replace(/\b,\b/g, ' '); // replace directly in string
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.replace(/\b,\b/g, ' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Execute query
+  const bootcamps = await query;
+
   return res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
 });
 
@@ -67,7 +100,7 @@ exports.getBootcampsInRadius = asyncHanlder(async (req, res, next) => {
   const lat = loc[0].latitude;
   const lng = loc[0].longitude;
 
-  // Calc radius using radians (Divide dist by radius of earth (3,963 mi or 6,378 km))
+  // Calc radius using radians (Divide distance by radius of earth (3,963 mi or 6,378 km))
   const radius = distance / 3963;
 
   const bootcamps = await Bootcamp.find({
