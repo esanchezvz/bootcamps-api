6e2f45096c5a8c02287879d6d8b883515a66d18b
esanchezvz <esteban.sanvaz@gmail.com>
2019-10-25 12:16:50 -0500

    update and delete courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -40,7 +40,7 @@ exports.getCourse = asyncHanlder(async (req, res, next) => {
 });
 
 // @desc Add a course
-// @route POST api/v1/bootcamps/:bootcampId/:courses
+// @route POST api/v1/bootcamps/:bootcampId/courses
 // @access Private
 exports.addCourse = asyncHanlder(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
@@ -54,4 +54,38 @@ exports.addCourse = asyncHanlder(async (req, res, next) => {
   const course = await Course.create(req.body);
 
   res.status(200).json({ success: true, data: course });
+});
+
+// @desc Update a course
+// @route PUT api/v1/courses/:id
+// @access Private
+exports.updateCourse = asyncHanlder(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+  delete req.body.bootcamp; // It cannot change botcamps
+
+  if (!course) {
+    next(new ErrorResponse(`No bootcamp with id of: ${req.params.id}`, 404));
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: course });
+});
+
+// @desc Delete a course
+// @route PUT api/v1/courses/:id
+// @access Private
+exports.deleteCourse = asyncHanlder(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    next(new ErrorResponse(`No bootcamp with id of: ${req.params.id}`, 404));
+  }
+
+  await course.remove();
+
+  res.status(200).json({ success: true, data: `Course deleted succesfuly.` });
 });
